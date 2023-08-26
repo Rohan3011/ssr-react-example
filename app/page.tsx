6449dfe3673aa916601f539132677e4b35c73443
rohan3011 <rohankamble3011@gmail.com>
2023-08-26 11:25:31 +0530

    Add forms

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import MyForm from "@/components/my-form";
+import { ProfileForm } from "@/components/my-form";
 import Posts from "@/components/posts";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-8 p-24">
-      <Posts />
-      <MyForm />
+      <ProfileForm />
     </main>
   );
 }
