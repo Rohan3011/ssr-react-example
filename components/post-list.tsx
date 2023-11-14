007d7d05bab601bb2c214ebf4d4c30c9f7957aba
rohan3011 <rohankamble3011@gmail.com>
2023-11-14 19:17:07 +0530

    fixed css

--- components/post-list.tsx
+++ components/post-list.tsx
@@ -14,8 +14,8 @@ export default async function PostList() {
   const posts = await prisma.post.findMany();
   return (
     <div className="flex flex-col p-8">
-      <h2>My Posts</h2>
-      <div>
+      <h2 className="text-2xl font-bold mb-4">My Posts</h2>
+      <div className="w-full flex flex-wrap gap-4">
         {posts.map((post) => (
           <Card key={post.id} className="w-[350px]">
             <CardHeader>
